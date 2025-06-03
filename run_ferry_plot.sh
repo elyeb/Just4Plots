0639ce580dd8f6668dee964dff9560eb9fd15d79
elyeb <elyebliss@gmail.com>
2025-06-03 16:36:19 -0700

    finalizing no-model plot code

---
+++ run_ferry_plot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/caffeinate -i /Library/Frameworks/Python.framework/Versions/3.9/bin/python3 /Users/elyebliss/Documents/Just4Plots/src/ferry_plot_model.py >> /Users/elyebliss/Documents/Just4Plots/ferry_plot.log 2>&1
--- src/ferry_plot_model.py
+++ src/ferry_plot_model.py
@@ -2,170 +2,412 @@
 Make a plot that is regularly updated with today's ferry data.
 
 """
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime
 import os
-
+import re
 import requests
 from bs4 import BeautifulSoup
 
-def get_ferry_schedule(dock,dest):
-    """
-    dock = "Colman"
-    dest = "Bainbridge"
-    """
-    # get today's ferry schedule
-    dock_dict = {
-        "Colman": 7,
-        "Bainbridge": 3,
-        "Kingston": 12,
-        "Edmonds": 8,
-    }
+DATA_FOLDER = os.path.join(
+    os.path.dirname(__file__), "../data/ferry_merged_space_delays/"
+)
+PLOT_FOLDER = os.path.join(os.path.dirname(__file__), "../outputs/plots/ferries/")
 
 
-    schedule_date = datetime.date.today().strftime("%Y%m%d")
-    request_url = f"https://www.wsdot.com/Ferries/Schedule/scheduledetail.aspx?tripdate={schedule_date}&departingterm={dock_dict[dock]}&arrivingterm={dock_dict[dest]}"
-
-    response = requests.get(request_url)
-    html_content = response.text
-    soup = BeautifulSoup(html_content, 'html.parser')
-    table = soup.find('table', class_='schedgrid')  # Replace with your table's class or other identifier
-
-    rows = table.find_all('tr')
-    schdeule_data = []
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        schdeule_data.append(cols)
-    schdeule_df = pd.DataFrame(schdeule_data)
-    schdeule_df.columns = ["time","vessel","null"]
-    schdeule_df = schdeule_df[~schdeule_df['time'].isnull()]
-    # covert times after PM to 24-hour format
-    schdeule_df = schdeule_df.reset_index(drop=True)
-    schdeule_df.drop(columns=["null"], inplace=True)
-    schedule_am_index = schdeule_df[schdeule_df["time"]=='AM'].index.tolist()[0]
-    schedule_pm_index = schdeule_df[schdeule_df["time"]=='PM'].index.tolist()[0]
-    midnight_index = schdeule_df[schdeule_df["time"].str.contains('AM ')].index.tolist()[0]
-    schdeule_df = schdeule_df[schdeule_df.index <midnight_index]
-    schedule_am = schdeule_df[schdeule_df.index<schedule_pm_index]
-    schedule_am = schedule_am[~schedule_am["vessel"].isnull()]
-    schedule_pm = schdeule_df[schdeule_df.index>schedule_pm_index]
-    def convert_to_24_hour_format(time_str):
-        hour = time_str.split(':')[0]
-        if hour == '12':  # Handle the case for 12 AM
-            return time_str  # 12 AM is already in 24-hour format
-        minute = time_str.split(':')[1]
-        new_hour = str(int(hour)+12)
-        new_time = f"{new_hour}:{minute}"
-        return new_time
-
-
-    schedule_pm["time"] = schedule_pm["time"].apply(convert_to_24_hour_format)
-
-    schdeule_df = pd.concat([schedule_am, schedule_pm], ignore_index=True)
-
-    return schdeule_df
-
-PLOT_DF_FOLDER = os.path.join(os.path.dirname(__file__), "../data/ferry_merged_space_delays/")
-
-df = pd.read_parquet(PLOT_DF_FOLDER + "ferry_merged_space_delays.parquet")
+df_full = pd.read_parquet(DATA_FOLDER + "ferry_merged_space_delays.parquet")
 
 today = datetime.date.today().strftime("%m/%d/%Y")
 
+day_of_week = datetime.date.today().strftime("%A")
 
 
+## Constants
+dock_dict_names = {
+    "Colman": "Seattle",
+    "Bainbridge": "Bainbridge",
+    "Kingston": "Kingston",
+    "Edmonds": "Edmonds",
+}
 
-day_of_week = today.strftime("%A")
 
-def plot_scatter_day(data, dock, dest, day_of_week,date, custom_title):
+def get_ferry_schedule(dock, dest, day_of_week):
     """
-    data = df.copy()
-    dock = "Colman"
-    dest = "Bainbridge"
+
+    dock = "Edmonds"
+    dest = "Kingston"
+    """
+
+    dock_dict_route = {
+        "Colman": "sea-bi",
+        "Bainbridge": "sea-bi",
+        "Kingston": "ed-king",
+        "Edmonds": "ed-king",
+    }
+
+    # get yesterday's bleeding over schedule
+    request_url = f"https://wsdot.com/ferries/schedule/scheduledetailbyroute.aspx?route={dock_dict_route[dock]}"
+
+    response = requests.get(request_url)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, "html.parser")
+    tables = soup.find(
+        "table", class_="schedgridbyroute"
+    )  # Replace with your table's class or other identifier
+
+    rows = tables.find_all("tr")
+    schdeule_data = []
+    for row in rows:
+        cols = []
+        tds = row.find_all("td")
+        for td in tds:
+            # Find divs with class 'am' or 'pm' inside each td
+            div = td.find("div", class_=["am", "pm"])
+            if div:
+                ampm = div.get("class")[0].upper()  # 'AM' or 'PM'
+                time_str = f"{div.text.strip()} {ampm}"
+                cols.append(time_str)
+            else:
+                # Fallback: just get the stripped text of the td
+                cols.append(td.text.strip())
+        schdeule_data.append(cols)
+    schdeule_df = pd.DataFrame(schdeule_data)
+
+    # depart and return shown on same page
+    depart_index_range = schdeule_df[
+        schdeule_df[0].str.contains(dock_dict_names[dock])
+    ].index.tolist()
+    return_index_range = schdeule_df[
+        schdeule_df[0].str.contains(dock_dict_names[dest])
+    ].index.tolist()
+
+    # helper functions:
+    def replace_time_str(time_str):
+        time_str = time_str.replace("12:", "00:")
+        time_str = time_str.replace("Midnight", "00:00")
+        return time_str
+
+    def convert_to_24_hour_format(time_str):
+        pattern = re.compile(r" AM| PM")
+        hour = time_str.split(":")[0]
+        if hour == "12":  # Handle the case for 12 AM
+            time_str = re.sub(pattern, "", time_str)  # Remove AM/PM
+            return time_str  # 12 AM is already in 24-hour format
+        if "PM" in time_str:
+
+            minute = time_str.split(":")[1]
+            new_hour = str(int(hour) + 12)
+            new_time = f"{new_hour}:{minute}"
+            time_str = re.sub(pattern, "", new_time)
+        else:
+            time_str = re.sub(pattern, "", time_str)
+        return time_str
+
+    if len(depart_index_range) == 1:
+        # Weekday and weekend schedule are the same
+        if depart_index_range[0] == 0:
+            schedule = schdeule_df.iloc[
+                (depart_index_range[0] + 1) : return_index_range[0]
+            ]
+        else:
+            schedule = schdeule_df.iloc[(depart_index_range[0] + 1) : len(schdeule_df)]
+
+        schedule[2] = schedule[2].apply(replace_time_str)
+        schedule[3] = schedule[3].apply(replace_time_str)
+
+        previous_overlap = (
+            schedule[schedule[2].str.contains("AM")][2].to_list()
+            + schedule[(schedule[3].str.contains("AM")) & (schedule[3] != "")][
+                3
+            ].to_list()
+        )
+        todays_schedule = (
+            schedule[0].to_list()
+            + schedule[1].to_list()
+            + schedule[~schedule[2].str.contains("AM")][2].to_list()
+            + schedule[(~schedule[3].str.contains("AM")) & (schedule[3] != "")][
+                3
+            ].to_list()
+        )
+        todays_schedule = previous_overlap + todays_schedule
+        todays_schedule = [
+            convert_to_24_hour_format(time_str) for time_str in todays_schedule
+        ]
+
+    else:
+        if depart_index_range[0] == 0:
+            weekday_schedule = schdeule_df.iloc[
+                (depart_index_range[0] + 1) : return_index_range[0]
+            ]
+            weekend_schedule = schdeule_df.iloc[
+                (depart_index_range[1] + 1) : return_index_range[1]
+            ]
+        else:
+            weekday_schedule = schdeule_df.iloc[
+                (depart_index_range[0] + 1) : return_index_range[1]
+            ]
+            weekend_schedule = schdeule_df.iloc[
+                (depart_index_range[1] + 1) : len(schdeule_df)
+            ]
+
+        weekday_schedule[2] = weekday_schedule[2].apply(replace_time_str)
+        weekday_schedule[3] = weekday_schedule[3].apply(replace_time_str)
+        weekend_schedule[2] = weekend_schedule[2].apply(replace_time_str)
+        weekend_schedule[3] = weekend_schedule[3].apply(replace_time_str)
+
+        if day_of_week in ["Saturday", "Sunday"]:
+            if day_of_week == "Saturday":
+                previous_overlap = (
+                    weekday_schedule[weekday_schedule[2].str.contains("AM")][
+                        2
+                    ].to_list()
+                    + weekday_schedule[
+                        (weekday_schedule[3].str.contains("AM"))
+                        & (weekday_schedule[3] != "")
+                    ][3].to_list()
+                )
+                todays_schedule = (
+                    weekend_schedule[0].to_list()
+                    + weekend_schedule[1].to_list()
+                    + weekend_schedule[~weekend_schedule[2].str.contains("AM")][
+                        2
+                    ].to_list()
+                    + weekend_schedule[
+                        (~weekend_schedule[3].str.contains("AM"))
+                        & (weekend_schedule[3] != "")
+                    ][3].to_list()
+                )
+                todays_schedule = previous_overlap + todays_schedule
+                todays_schedule = [
+                    convert_to_24_hour_format(time_str) for time_str in todays_schedule
+                ]
+            else:
+                previous_overlap = (
+                    weekend_schedule[weekend_schedule[2].str.contains("AM")][
+                        2
+                    ].to_list()
+                    + weekend_schedule[
+                        (weekend_schedule[3].str.contains("AM"))
+                        & (weekend_schedule[3] != "")
+                    ][3].to_list()
+                )
+                todays_schedule = (
+                    weekend_schedule[0].to_list()
+                    + weekend_schedule[1].to_list()
+                    + weekend_schedule[~weekend_schedule[2].str.contains("AM")][
+                        2
+                    ].to_list()
+                    + weekend_schedule[
+                        (~weekend_schedule[3].str.contains("AM"))
+                        & (weekend_schedule[3] != "")
+                    ][3].to_list()
+                )
+                todays_schedule = previous_overlap + todays_schedule
+                todays_schedule = [
+                    convert_to_24_hour_format(time_str) for time_str in todays_schedule
+                ]
+        else:
+            if day_of_week == "Monday":
+                previous_overlap = (
+                    weekend_schedule[weekend_schedule[2].str.contains("AM")][
+                        2
+                    ].to_list()
+                    + weekend_schedule[
+                        (weekend_schedule[3].str.contains("AM"))
+                        & (weekend_schedule[3] != "")
+                    ][3].to_list()
+                )
+                todays_schedule = (
+                    weekday_schedule[0].to_list()
+                    + weekday_schedule[1].to_list()
+                    + weekday_schedule[~weekday_schedule[2].str.contains("AM")][
+                        2
+                    ].to_list()
+                    + weekday_schedule[
+                        (~weekday_schedule[3].str.contains("AM"))
+                        & (weekday_schedule[3] != "")
+                    ][3].to_list()
+                )
+                todays_schedule = previous_overlap + todays_schedule
+                todays_schedule = [
+                    convert_to_24_hour_format(time_str) for time_str in todays_schedule
+                ]
+            else:
+                previous_overlap = (
+                    weekday_schedule[weekday_schedule[2].str.contains("AM")][
+                        2
+                    ].to_list()
+                    + weekday_schedule[
+                        (weekday_schedule[3].str.contains("AM"))
+                        & (weekday_schedule[3] != "")
+                    ][3].to_list()
+                )
+                todays_schedule = (
+                    weekday_schedule[0].to_list()
+                    + weekday_schedule[1].to_list()
+                    + weekday_schedule[~weekday_schedule[2].str.contains("AM")][
+                        2
+                    ].to_list()
+                    + weekday_schedule[
+                        (~weekday_schedule[3].str.contains("AM"))
+                        & (weekday_schedule[3] != "")
+                    ][3].to_list()
+                )
+                todays_schedule = previous_overlap + todays_schedule
+                todays_schedule = [
+                    convert_to_24_hour_format(time_str) for time_str in todays_schedule
+                ]
+
+    # previous method also shows which vessels are planned. Good to keep for modeling stage
+
+    # get today's ferry schedule
+    # dock_dict_nums = {
+    #     "Colman": 7,
+    #     "Bainbridge": 3,
+    #     "Kingston": 12,
+    #     "Edmonds": 8,
+    # }
+    # schedule_date = datetime.date.today().strftime("%Y%m%d")
+    # previous_date = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y%m%d")
+
+    # get today's schedule
+    # request_url = f"https://www.wsdot.com/Ferries/Schedule/scheduledetail.aspx?tripdate={schedule_date}&departingterm={dock_dict[dock]}&arrivingterm={dock_dict[dest]}"
+
+    # response = requests.get(request_url)
+    # html_content = response.text
+    # soup = BeautifulSoup(html_content, 'html.parser')
+    # table = soup.find('table', class_='schedgrid')  # Replace with your table's class or other identifier
+
+    # rows = table.find_all('tr')
+    # schdeule_data = []
+    # for row in rows:
+    #     cols = row.find_all('td')
+    #     cols = [ele.text.strip() for ele in cols]
+    #     schdeule_data.append(cols)
+    # schdeule_df = pd.DataFrame(schdeule_data)
+    # schdeule_df.columns = ["time","vessel","null"]
+    # schdeule_df = schdeule_df[~schdeule_df['time'].isnull()]
+    # # covert times after PM to 24-hour format
+    # schdeule_df = schdeule_df.reset_index(drop=True)
+    # schdeule_df.drop(columns=["null"], inplace=True)
+    # schedule_am_index = schdeule_df[schdeule_df["time"]=='AM'].index.tolist()[0]
+    # schedule_pm_index = schdeule_df[schdeule_df["time"]=='PM'].index.tolist()[0]
+    # midnight_index = schdeule_df[schdeule_df["time"].str.contains('AM ')].index.tolist()[0]
+    # schdeule_df = schdeule_df[schdeule_df.index <midnight_index]
+    # schedule_am = schdeule_df[schdeule_df.index<schedule_pm_index]
+    # schedule_am = schedule_am[~schedule_am["vessel"].isnull()]
+    # schedule_pm = schdeule_df[schdeule_df.index>schedule_pm_index]
+
+    # def convert_to_24_hour_format(time_str):
+    #     hour = time_str.split(':')[0]
+    #     if hour == '12':  # Handle the case for 12 AM
+    #         return time_str  # 12 AM is already in 24-hour format
+    #     minute = time_str.split(':')[1]
+    #     new_hour = str(int(hour)+12)
+    #     new_time = f"{new_hour}:{minute}"
+    #     return new_time
+
+    # schedule_pm["time"] = schedule_pm["time"].apply(convert_to_24_hour_format)
+
+    # schdeule_df = pd.concat([schedule_am, schedule_pm], ignore_index=True)
+
+    return todays_schedule
+
+
+def plot_scatter_day(data, dock, dest, day_of_week, date):
+    """
+    data = df_full.copy()
+    dock = "Bainbridge"
+    dest = "Colman"
     day_of_week = day_of_week
     date = today
-    custom_title = day_title
+
     """
+    graph_title = (
+        f"{dock_dict_names[dock]} to {dock_dict_names[dest]}, {day_of_week} {date}"
+    )
+    filename_date = date.replace("/", "-")
+    filename = f"{dock_dict_names[dock]}_to_{dock_dict_names[dest]}_{day_of_week}_{filename_date}.png"
+    # filter
     df = data[data["Destination"] == dest]
     df = df[df["Departing"] == dock]
-
     df = df[df["day_of_week"] == day_of_week]
-
     df = df.sort_values(by="scheduled_depart")
-    
-    df["scheduled_depart"] = pd.to_datetime(df["scheduled_depart"], format="%H:%M")
 
+    # format
+    df["scheduled_depart"] = pd.to_datetime(df["scheduled_depart"], format="%H:%M")
 
     today_df = df[df["Date"] == date]
     prev_df = df[df["Date"] != date]
 
-    depart_times = prev_df[prev_df["Date"]==prev_df["Date"].max()]["scheduled_depart"] #.dt.strftime("%H:%M")
+    depart_times = get_ferry_schedule(dock, dest, day_of_week)
+    depart_times = pd.to_datetime(depart_times, format="%H:%M")
 
     fig, ax = plt.subplots(figsize=(12, 6))
     # Plot depart_dif above the x-axis
 
-
     dots_1_historic = plt.plot(
         prev_df["scheduled_depart"].dt.strftime("%H:%M"),
-        prev_df["depart_dif"], 
-        'o',
-        label="Dif btw scheduled & actual departure (historic)",
+        prev_df["depart_dif"],
+        "o",
+        label="Actual departure (historic)",
         color="blue",
+        markeredgecolor="none",
         alpha=0.2,
-             ) 
+    )
     dots_1_today = plt.plot(
         today_df["scheduled_depart"].dt.strftime("%H:%M"),
         today_df["depart_dif"],
-         'o',
-        label="Dif btw scheduled & actual departure (today)",
+        "o",
+        label="Actual departure (today)",
         color="black",
         alpha=1,
-             )
+    )
 
     dots_2_historic = plt.plot(
         prev_df["scheduled_depart"].dt.strftime("%H:%M"),
         prev_df["soldout_dif"],
-         'o',
-        label="Dif btw scheduled departure & sellout time (historic)",
+        "o",
+        label="Sellout time (historic)",
         color="red",
+        markeredgecolor="none",
         alpha=0.2,
-             )  
+    )
 
     dots_2_today = plt.plot(
         today_df["scheduled_depart"].dt.strftime("%H:%M"),
         today_df["soldout_dif"],
-         'x',
-        label="Dif btw scheduled departure & sellout time (today)",
+        "x",
+        label="Sellout time (today)",
         color="black",
         alpha=1,
-             )
-    
-    ax.set_xticks(depart_times.dt.strftime("%H:%M"))
-    ax.set_xticklabels(depart_times.dt.strftime("%H:%M"), rotation=45, fontsize=8)
-   
+    )
+
+    ax.spines["bottom"].set_position(("data", 0))
+    ax.set_xticks(depart_times.strftime("%H:%M"))
+    ax.set_xticklabels(depart_times.strftime("%H:%M"), rotation=45, fontsize=8)
+
     # Customize each subplot
-    # ax.set_title(day_of_week, fontsize=12)
-    # ax.axhline(
-    #     0, color="black", linewidth=0.8, linestyle="--"
-    # )  # Add a horizontal line at y=0
+    ax.set_title(day_of_week, fontsize=12)
+    ax.axhline(
+        0, color="black", linewidth=0.8, linestyle="--"
+    )  # Add a horizontal line at y=0
 
-    # # ax.tick_params(axis="x", rotation=45, labelsize=8)
+    ax.set_ylabel("Difference (Minutes)", fontsize=10)
 
-    # # Keep x-ticks on the y=0 line
-    # ax.spines["bottom"].set_position(("data", 0))
-
-    # ax.set_ylabel("Difference (Minutes)", fontsize=10)
-
-    # ax.legend(loc="upper left", fontsize=10)
+    ax.legend(loc="upper left", fontsize=10)
     # # Adjust layout
-    # # fig.suptitle(custom_title)
-    # fig.text(0.5, -0.05, "Scheduled Departure Time", ha="center", fontsize=12)
+    fig.suptitle(graph_title)
+    fig.text(0.5, -0.05, "Scheduled Departure Time", ha="center", fontsize=12)
     plt.tight_layout()
-    # fig.savefig(outfile_day, bbox_inches="tight", dpi=300, facecolor="white")
+    fig.savefig(PLOT_FOLDER + filename, bbox_inches="tight", dpi=300, facecolor="white")
     plt.show()
 
 
-# plot_scatter_day(data, dock, dest, outfile_day, day_of_week, custom_title)
-plot_scatter_day(df,"Colman","Bainbridge",f"{TODAY}","colman_bainbridge_{TODAY}.png")
+plot_scatter_day(df_full, "Colman", "Bainbridge", day_of_week, today)
+plot_scatter_day(df_full, "Bainbridge", "Colman", day_of_week, today)
+plot_scatter_day(df_full, "Edmonds", "Kingston", day_of_week, today)
+plot_scatter_day(df_full, "Kingston", "Edmonds", day_of_week, today)
